4460d95673a74bfe201162bfb494420b52e11670
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-05-02 10:42:24 +0200

    first req

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const hello = "World";
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://site--backend-deliveroo--75fxzbtlk68n.code.run/"
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? <p>Loading...</p> : <div></div>;
 }
 
 export default App;
